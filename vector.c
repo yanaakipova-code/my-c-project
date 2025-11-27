04000ef603f69ceab6288f7a866124dd729dc483
Яна <yanaakipova@gmail.com>
2025-11-27 15:17:50 +0300

    добавила функции для доступа к элементам

--- vector.c
+++ vector.c
@@ -21,3 +21,35 @@ vector* vector_create() {
     temp_vector->capacity = initial_capacity;
     return temp_vector;
 }
+void vector_clear(vector* vec) {
+    free(vec->data);
+    vec->data = NULL;
+    vec->size = 0;
+    vec->capacity = 0;
+}
+void vector_destroy(vector* vec) {
+    if (vec == NULL) return;
+    vector_clear(vec);
+    free(vec);    
+}
+size_t vector_size(const vector* vec) {
+    if (vec == NULL) {
+        return 0; 
+    }
+    return vec->size;   
+}
+apartment_building* vector_get(const vector* vec, size_t index){
+    if (vec == NULL || index >= vec->size) {
+        return NULL;
+    }    
+    return &vec->data[index];
+}
+apartment_building* vector_front(const vector* vec) {
+    return vector_get(vec, 0);
+}
+apartment_building* vector_back(const vector* vec) {
+    if (vec == NULL || vec->size == 0){
+        return NULL;
+    }
+    return &vec->data[vec->size - 1];
+} 
