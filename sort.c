f6a5e265baa81f7490534a63994891c75a1e5491
Яна <yanaakipova@gmail.com>
2025-11-30 23:33:27 +0300

    сортировка по всем пунктам

--- sort.c
+++ sort.c
@@ -4,7 +4,7 @@ void selection_sort(vector* vec, Comparator comp) {
     if (!vector_is_valid_and_not_empty(vec)) {
         return;
     }
-
+    
     size_t n = vector_size(vec);
 
     for (size_t i = 0; i < n - 1; i++){
@@ -25,4 +25,51 @@ void selection_sort(vector* vec, Comparator comp) {
         }
     }
 
+}
+bool compare_by_year(const apartment_building* a, const apartment_building* b) {
+    return a->year < b->year;
+}
+
+bool compare_by_year_desc(const apartment_building* a, const apartment_building* b) {
+    return a->year > b->year;
+}
+
+bool compare_by_apartments(const apartment_building* a, const apartment_building* b) {
+    return a->count_apartments < b->count_apartments;
+}
+
+bool compare_by_apartments_desc(const apartment_building* a, const apartment_building* b) {
+    return a->count_apartments > b->count_apartments;
+}
+
+bool compare_by_floors(const apartment_building* a, const apartment_building* b) {
+    return a->count_floors < b->count_floors;
+}
+
+bool compare_by_floors_desc(const apartment_building* a, const apartment_building* b) {
+    return a->count_floors > b->count_floors;
+}
+
+bool compare_by_area(const apartment_building* a, const apartment_building* b) {
+    return a->area_apartment < b->area_apartment;
+}
+
+bool compare_by_area_desc(const apartment_building* a, const apartment_building* b) {
+    return a->area_apartment > b->area_apartment;
+}
+
+bool compare_by_developer(const apartment_building* a, const apartment_building* b) {
+    return strcmp(a->developer, b->developer) < 0;
+}
+
+bool compare_by_developer_desc(const apartment_building* a, const apartment_building* b) {
+    return strcmp(a->developer, b->developer) > 0;
+}
+
+bool compare_by_microdistrict(const apartment_building* a, const apartment_building* b) {
+    return strcmp(a->microdistrict, b->microdistrict) < 0;
+}
+
+bool compare_by_microdistrict_desc(const apartment_building* a, const apartment_building* b) {
+    return strcmp(a->microdistrict, b->microdistrict) > 0;
 }
