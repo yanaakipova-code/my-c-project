969fa62c78d7bbe08e8ca09ce538b9470a59fe9d
Яна <yanaakipova@gmail.com>
2025-11-30 23:33:49 +0300

    все объявление

--- sort.h
+++ sort.h
@@ -4,3 +4,16 @@
 
 typedef bool (*Comparator)(const apartment_building*, const apartment_building*);
 void selection_sort(vector* vec, Comparator comp);
+
+bool compare_by_year(const apartment_building* a, const apartment_building* b);
+bool compare_by_year_desc(const apartment_building* a, const apartment_building* b);
+bool compare_by_apartments(const apartment_building* a, const apartment_building* b);
+bool compare_by_apartments_desc(const apartment_building* a, const apartment_building* b);
+bool compare_by_floors(const apartment_building* a, const apartment_building* b);
+bool compare_by_floors_desc(const apartment_building* a, const apartment_building* b);
+bool compare_by_area(const apartment_building* a, const apartment_building* b);
+bool compare_by_area_desc(const apartment_building* a, const apartment_building* b);
+bool compare_by_developer(const apartment_building* a, const apartment_building* b);
+bool compare_by_developer_desc(const apartment_building* a, const apartment_building* b);
+bool compare_by_microdistrict(const apartment_building* a, const apartment_building* b);
+bool compare_by_microdistrict_desc(const apartment_building* a, const apartment_building* b);
