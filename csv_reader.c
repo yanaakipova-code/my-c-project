d8f5f3b87443f180ae954554d1ef795ac574690d
Яна <yanaakipova@gmail.com>
2025-12-02 00:39:45 +0300

    добавлена функция для записи данных

--- csv_reader.c
+++ csv_reader.c
@@ -7,15 +7,23 @@ static bool parse_bool(const char *str){
     if (strcmp(str, "true") || strcmp(str, "yes") || strcmp(str, "1")){
         return true;
     }
+    else{
+        return false;
+    }
 }
 
 vector* read_buildings_from_csv(const char * filename){
-    FILE* file = foprn(filename, "r");
+    FILE* file = fopen(filename, "r");
 
-    if (filename == NULL){
+    if (file == NULL){
         printf("Error: file: %s couldn't be opened", filename);
         return NULL;
     }
-
+    vector* buildings = vector_create();
+    if (buildings == NULL){
+        fclose(file);
+        puts("Error: couldn't write file");
+        return NULL;
+    }
 }
 
