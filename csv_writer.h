9345d711d552d208b96e0422879eaa9ebe2b2d5e
Яна <yanaakipova@gmail.com>
2025-12-04 22:50:05 +0300

    объявление функций

--- csv_writer.h
+++ csv_writer.h
@@ -1,2 +1,6 @@
 #pragma once
 #include "vector.h"
+
+void save_buildings_to_csv(const vector* buildings, const char* filename);
+
+void write_buildings_to_stream(const vector* buildings, FILE* stream);
