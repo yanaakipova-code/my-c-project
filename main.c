359cd9a07638e77f034deda553f9902f7b7d7fb6
Яна <yanaakipova@gmail.com>
2025-12-05 13:13:32 +0300

    по какому полю сортируем

---
+++ main.c
@@ -0,0 +1,80 @@
+#include "args.h"
+#include "apartment_building.h"
+#include "vector.h"
+#include "csv_reader.h"
+#include "csv_writer.h"
+#include "sort.h"
+#include "building_generator.h"
+#include "table_printer.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static Comparator get_comparator_by_field(SortOrder order, const char* field_name){
+    
+    if (field_name == NULL){
+        if (order == ORDER_ASC){
+            return compare_by_year;
+        }else{
+            return compare_by_year_desc;
+        }
+    }
+
+     if (strcmp(field_name, "developer") == 0) {
+        if (order == ORDER_ASC) {
+            return compare_by_developer;
+        } else {
+            return compare_by_developer_desc;
+        }
+    }
+
+    if (strcmp(field_name, "microdistrict") == 0) {
+        if (order == ORDER_ASC) {
+            return compare_by_microdistrict;
+        } else {
+            return compare_by_microdistrict_desc;
+        }
+    }
+
+    if (strcmp(field_name, "year") == 0) {
+        if (order == ORDER_ASC) {
+            return compare_by_year;
+        } else {
+            return compare_by_year_desc;
+        }
+    }
+
+    if (strcmp(field_name, "apartments") == 0) {
+        if (order == ORDER_ASC) {
+            return compare_by_apartments;
+        } else {
+            return compare_by_apartments_desc;
+        }
+    }
+
+    if (strcmp(field_name, "floors") == 0) {
+        if (order == ORDER_ASC) {
+            return compare_by_floors;
+        } else {
+            return compare_by_floors_desc;
+        }
+    }
+
+    if (strcmp(field_name, "area") == 0) {
+        if (order == ORDER_ASC) {
+            return compare_by_area;
+        } else {
+            return compare_by_area_desc;
+        }
+    }
+
+    if (strcmp(field_name, "type") == 0) {
+        if (order == ORDER_ASC) {
+            return compare_by_type;
+        } else {
+            return compare_by_type_desc;
+        }
+    }
+}
+
