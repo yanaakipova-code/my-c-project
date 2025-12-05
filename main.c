12c005d016db9ca374bc1cf24dd087b97f04a300
Яна <yanaakipova@gmail.com>
2025-12-05 13:15:31 +0300

    генерация домов

--- main.c
+++ main.c
@@ -78,3 +78,34 @@ static Comparator get_comparator_by_field(SortOrder order, const char* field_nam
     }
 }
 
+
+static int data_generation(const ProgramArgs* args){
+    if (args->generate_count < 0) {
+        return 1;
+    }
+
+    vector* buildings = vector_create();
+    if (buildings == NULL) {
+        return 1;
+    }
+
+    for (int i = 0; i < args->generate_count; i++) {
+        apartment_building building = generate_random_building();
+        vector_push_back(buildings, &building);
+    }
+
+    if (args->output_file) {
+        if (write_buildings_to_csv(buildings, args->output_file) == NULL) {
+            fprintf(stderr, "Ошибка: не удалось записать данные в файл '%s'\n", args->output_file);
+            vector_destroy(buildings);
+            return 1;
+        }
+        printf("Данные успешно сохранены в файл: %s\n", args->output_file);
+    } else {
+        puts("Сгенерированные данные (CSV формат):\n");
+        write_buildings_to_csv_stream(buildings, stdout);
+    }
+    vector_destroy(buildings);
+
+    return 1;
+}
