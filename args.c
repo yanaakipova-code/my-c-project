e22dddd0288c49c3426ad12311f3710b7afbc337
Яна <yanaakipova@gmail.com>
2025-12-05 13:11:35 +0300

    сортировка через параметр

--- args.c
+++ args.c
@@ -9,6 +9,7 @@
 bool parse_arguments(int argc, char* argv[], ProgramArgs* args){
 args->mode = MODE_ERROR;
 args->order = ORDER_ASC;
+args->sort_field = NULL;
 args->input_file = NULL;
 args->output_file = NULL;
 args->generate_count = 0;
@@ -29,8 +30,6 @@ for (int i = 1; i < argc; i++) {
                 return false;  
             }
         }
-
-
         else if (strcmp(argv[i], "--sort") == 0 || strcmp(argv[i], "-s") == 0) {
             if (args->mode != MODE_ERROR) {
                 puts("Error: Multiple modes specified\n");
@@ -99,7 +98,12 @@ for (int i = 1; i < argc; i++) {
                     return false;
                 }
         }
-        
+        else if (strncmp(argv[i], "--field=", 8) == 0) {
+            args->sort_field = argv[i] + 8;
+        }
+        else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
+            args->sort_field = argv[++i];
+        }
         else {
             printf("Error: Unknown argument '%s'\n", argv[i]);
             return false;
