6a116cb4cc7e897542229f27ae11f302d722dc98
Яна <yanaakipova@gmail.com>
2025-12-09 19:03:05 +0300

    Справка о возможных действиях

---
+++ args_parser.c
@@ -0,0 +1,134 @@
+#include <stdbool.h>
+#include <string.h> 
+#include <stddef.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include "args_parser.h"
+
+#define PASS_ONE 6
+#define PASS_TWO 5
+
+static void print_help(const char* program_name){
+    printf("Использование: %s [РЕЖИМ] [ЗНАЧЕНИЯ]", program_name);
+    puts("Режимы(выберите один)");
+    puts("  --generate N, -g N   Сгенерировать N случайных зданий\n");
+    puts("  --sort, -s           Отсортировать здания\n");
+    puts("  --print, -P          Напечатать здания в виде таблицы\n\n");
+    
+    puts("Опции:\n");
+    puts("  --in ФАЙЛ, -i ФАЙЛ   Входной файл (stdin если не указан)\n");
+    puts("  --out ФАЙЛ, -o ФАЙЛ  Выходной файл (stdout если не указан)\n");
+    puts("  --type asc|desc, -t A|D  Порядок сортировки (по умолчанию asc)\n");
+}
+
+
+bool parse_arguments(int argc, char* argv[], ProgramArgs* args){
+args->mode = MODE_ERROR;
+args->order = ORDER_ASC;
+args->sort_field = NULL;
+args->input_file = NULL;
+args->output_file = NULL;
+args->generate_count = 0;
+
+for (int i = 1; i < argc; i++) {
+        
+        if (strcmp(argv[i], "--generate") == 0 || strcmp(argv[i], "-g") == 0) {
+            if (args->mode != MODE_ERROR) {
+                puts("Ошибка: задано несколько режимов\n");
+                return false;
+            }
+            if (i + 1 < argc) {
+                args->mode = MODE_GENERATE;
+                i++;
+                args->generate_count = atoi(argv[i]);
+            } else {
+                puts("Ошибка: укажите количество для --generate\n");
+                return false;  
+            }
+        }
+        else if (strcmp(argv[i], "--sort") == 0 || strcmp(argv[i], "-s") == 0) {
+            if (args->mode != MODE_ERROR) {
+                puts("Ошибка: задано несколько режимов\n");
+                return false;
+            }
+            args->mode = MODE_SORT;
+        }
+
+
+        else if (strcmp(argv[i], "--print") == 0 || strcmp(argv[i], "-P") == 0) {
+            if (args->mode != MODE_ERROR) {
+                puts("Ошибка: задано несколько режимов\n");
+                return false;
+            }
+            args->mode = MODE_PRINT;
+        }
+
+        else if(strcmp(argv[i], "-o") == 0){
+            if (i+1 < argc){
+                args->output_file = argv[++i];
+            }else {
+                puts("Ошибка: -o требуется имя файла\n");
+                return false;
+            }
+        }
+        else if (strncmp(argv[i], "--out=", PASS_ONE) == 0){
+            args->output_file = argv[i] + PASS_ONE;
+        }
+
+
+        else if (strcmp(argv[i], "-i") == 0){
+            if (i+1 < argc){
+                args->input_file=argv[++i];
+            }
+            else{
+                puts("Ошибка: -i требуется имя файла\n");
+                return false;
+            }
+        }
+        else if(strncmp(argv[i], "--in=",PASS_TWO) == 0){
+            args->input_file = argv[i] + PASS_TWO;
+        }
+
+
+        else if (strcmp(argv[i], "--type=asc") == 0) {
+            args->order = ORDER_ASC;
+        }
+        else if (strcmp(argv[i], "--type=desc") == 0) {
+            args->order = ORDER_DESC;
+        }
+        else if (strcmp(argv[i], "-t") == 0) {
+            if (i + 1 < argc) {
+                if (strcmp(argv[i + 1], "A") == 0) {
+                    args->order = ORDER_ASC;
+                    i++;
+                }else if (strcmp(argv[i + 1], "D") == 0) {
+                    args->order = ORDER_DESC;
+                    i++;
+                }else{
+                    puts("Ошибка: -t вводится через A (asc) или D (desc)\n");
+                    return false;
+                }
+            }
+            else{
+                    puts("Ошибка: -t вводится через A (asc) или D (desc))\n");
+                    return false;
+                }
+        }
+        else if (strncmp(argv[i], "--field=", 8) == 0) {
+            args->sort_field = argv[i] + 8;
+        }
+        else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
+            args->sort_field = argv[++i];
+        }
+        else {
+            printf("Ошибка: Неизвестный аргумент '%s'\n", argv[i]);
+            return false;
+        }
+    }
+    if (args->mode == MODE_ERROR) {
+        printf("Ошибка: Режим не выбран\n");
+        return false;
+    }
+    
+    return true;
+}
