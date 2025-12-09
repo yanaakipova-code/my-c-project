fae4f740329e14e8e63380a8242ff826763d5205
Яна <yanaakipova@gmail.com>
2025-12-09 12:29:34 +0300

    объявление компаратора

--- sort.h
+++ sort.h
@@ -6,4 +6,4 @@ typedef int (*Comparator)(const apartment_building*, const apartment_building*);
 void selection_sort(vector* vec, Comparator comp);
 
 int comparator_by_ascending(const apartment_building *a, const apartment_building *b);
-int comporator_reductionconst( apartment_building *a, const apartment_building *b);
+int comporator_reduction(const apartment_building *a, const apartment_building *b);
