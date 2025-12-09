696e415fc1e13633c9208c19ceb50c8d801305a8
Яна <yanaakipova@gmail.com>
2025-12-09 19:25:39 +0300

    переименовала фаил

---
+++ args_parser.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdbool.h>  
+typedef enum{
+    MODE_ERROR,
+    MODE_GENERATE,
+    MODE_SORT,
+    MODE_PRINT
+}ProgramMode;
+typedef enum {
+    ORDER_ASC,
+    ORDER_DESC
+} SortOrder;
+typedef struct {
+    ProgramMode mode;
+    SortOrder order;
+    char* sort_field; 
+    char* input_file;
+    char* output_file;
+    int generate_count;
+} ProgramArgs;
+bool parse_arguments(int argc, char* argv[], ProgramArgs* args);
