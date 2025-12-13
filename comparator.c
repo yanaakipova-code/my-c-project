95aa7eaab2dd699cc43feb24afac293bc393ed44
Яна <yanaakipova@gmail.com>
2025-12-13 11:46:27 +0300

    более компактный компаратор

--- comparator.c
+++ comparator.c
@@ -2,72 +2,42 @@
 
 int comparator_by_ascending(const apartment_building *a, const apartment_building *b) {
     if (a->year != b->year) {
-        if (a->year < b->year) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return (a->year - b->year);
     }
     
     if (a->count_floors != b->count_floors) {
-        if (a->count_floors < b->count_floors) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return (a->count_floors - b->count_floors);
     }
     
     if (a->count_apartments != b->count_apartments) {
-        if (a->count_apartments < b->count_apartments) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return (a->count_apartments - b->count_apartments);
     }
     
     if (a->area_apartment != b->area_apartment) {
-        if (a->area_apartment < b->area_apartment) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return (a->area_apartment - b->area_apartment);
     }
     
     int cmp = strcmp(a->type, b->type);
-    if (cmp < 0) {
-        return -1;
-    } else if (cmp > 0) {
-        return 1;
+    if (cmp != 0){
+        return cmp;
     }
 
     int cmp_mic = strcmp(a->microdistrict, b->microdistrict);
-    if (cmp_mic < 0) {
-        return -1;
-    } else if (cmp_mic > 0) {
-        return 1;
+    if (cmp_mic != 0) {
+        return cmp_mic;
     }
     
     int cmp_dev = strcmp(a->developer, b->developer);
-    if (cmp_dev < 0) {
-        return -1;
-    } else if (cmp_dev > 0) {
-        return 1;
+    if (cmp_dev != 0) {
+        return cmp_dev;
     }
     
     if (a->elevator != b->elevator) {
-        if (a->elevator < b->elevator) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return (a->elevator - b->elevator);
     }
     
     if (a->garbage_chute != b->garbage_chute) {
-        if (a->garbage_chute < b->garbage_chute) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return (a->garbage_chute - b->garbage_chute);
     }
 
     return 0;
