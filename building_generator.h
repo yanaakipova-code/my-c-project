55210cc000456434c31f8410a56499ae7677a3f8
Яна <yanaakipova@gmail.com>
2025-12-17 11:01:28 +0300

    объявление

--- building_generator.h
+++ building_generator.h
@@ -1,5 +1,5 @@
 #pragma once
 #include "apartment_building.h"
 
-apartment_building generate_random_building(void);
+apartment_building generate_random_building();
 void generate_random_building_cvs(unsigned int count_home, const char *file_name);
